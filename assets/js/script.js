eac9d933f2be2bd6d05a2c6e1f805a86b0906789
Valerianavo <lilaj2sy@gmail.com>
2025-07-25 18:47:05 -0500

    banner-pasando-js

---
+++ assets/js/script.js
@@ -0,0 +1,26 @@
+// let i = 1;
+
+// function desvanecer() {
+//   const capa = document.getElementById("capa");
+//   capa.style.opacity = "0"; 
+//   setTimeout(girar, 1500);
+// }
+
+// function girar() {
+//   i++;
+//   if (i > 5) i = 1;
+
+//   const capa = document.getElementById("capa");
+//   capa.style.backgroundImage = `url("assets/img/banner${i}.jpg")`;
+//   capa.style.opacity = "1"; 
+
+//   setTimeout(desvanecer, 3500); 
+// }
+
+// window.onload = function () {
+//   const capa = document.getElementById("capa");
+//   capa.style.backgroundImage = `url("assets/img/banner${i}.jpg")`;
+//   setTimeout(desvanecer, 3500);
+// };
+
+
